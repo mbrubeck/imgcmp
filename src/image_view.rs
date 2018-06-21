934c3571ab6146a9d3cc5ca8c4aa16feabedef6b
Dan Kaplun <dbkaplun@gmail.com>
2018-06-20 20:12:43 -0400

    wip

--- src/image_view.rs
+++ src/image_view.rs
@@ -5,7 +5,7 @@ use util::color_avg;
 
 /// FIXME: remove and replace when GenericImageView: ?Sized
 pub trait ImageView<I: GenericImageView> {
-    fn view(&self, x: u32, y: u32, width: u32, height: u32) -> Self;
+    fn view(&self, x: u32, y: u32, width: u32, height: u32) -> Box<Self>;
     fn color_avg(&self) -> I::Pixel;
     fn bounds(&self) -> (u32, u32, u32, u32);
     fn dimensions(&self) -> (u32, u32) {
@@ -15,14 +15,15 @@ pub trait ImageView<I: GenericImageView> {
 }
 
 impl<I: GenericImageView> ImageView<I> for I
-where I::InnerImageView: GenericImageView<InnerImageView = I>,
+where
+    I::InnerImageView: GenericImageView<InnerImageView = I>,
 {
     fn bounds(&self) -> (u32, u32, u32, u32) {
         self.bounds()
     }
 
-    fn view(&self, x: u32, y: u32, w: u32, h: u32) -> Self {
-        self.view(x, y, w, h)
+    fn view(&self, x: u32, y: u32, w: u32, h: u32) -> Box<Self> {
+        Box::new(self.view(x, y, w, h))
     }
 
     fn color_avg(&self) -> I::Pixel {
@@ -38,8 +39,8 @@ where
         <Self as GenericImageView>::bounds(self)
     }
 
-    fn view(&self, x: u32, y: u32, w: u32, h: u32) -> Self {
-        <Self as GenericImageView>::view(self, x, y, w, h)
+    fn view(&self, x: u32, y: u32, w: u32, h: u32) -> Box<Self> {
+        Box::new(<Self as GenericImageView>::view(self, x, y, w, h))
     }
 
     fn color_avg(&self) -> I::Pixel {
